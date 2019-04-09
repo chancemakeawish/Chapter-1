8ddff7a0174c39df6a47ee51ffd0e7b4fe2e608a
Chance NIffen <ozzyniffen@gmail.com>
2019-04-09 12:37:46 -0500

    Initial commit

---
+++ src/TableAndChairs.java
@@ -0,0 +1,23 @@
+public class TableAndChairs {
+    //Author: Chance
+    //Date: 04/09/2019
+    //Projects: Tables and Chairs
+
+    public static void main(String[] Args){
+
+        System.out.println("Building Some Tables And Chairs");
+        System.out.println("\n\n\n\n");
+
+        System.out.println("C                                                C");
+        System.out.println("H                                                H");
+        System.out.println("A                                                A");
+        System.out.println("I      TABLETABLETABLETABLETABLETABLETABLE       I");
+        System.out.println("R             L                   L              R");
+        System.out.println("CHAIRCHAIR    E                   E     CHAIRCHAIR");
+        System.out.println("C        C    G                   G     C        C");
+        System.out.println("H        H    L                   L     H        H");
+        System.out.println("A        A    E                   E     A        A");
+        System.out.println("I        I    G                   G     I        I");
+        System.out.println("R        R    !                   !     R        R");
+    }
+}
