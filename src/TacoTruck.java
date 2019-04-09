e536a9eb51fa0227f937268081afddade9d0d4f1
Chance NIffen <ozzyniffen@gmail.com>
2019-04-09 13:21:07 -0500

    updadte tacos

---
+++ src/TacoTruck.java
@@ -0,0 +1,24 @@
+public class TacoTruck {
+
+    public static void main(String[] args) {
+
+        System.out.println("Taco Truck");
+
+
+        System.out.println("________________________________________");
+        System.out.println("|                                      | ");
+        System.out.println("|                                      |");
+        System.out.println("|                                      |");
+        System.out.println("|                                      |");
+        System.out.println("|                                      |");
+        System.out.println("|                                      |______________");
+        System.out.println("|                                                     |");
+        System.out.println("|                                                     |");
+        System.out.println("|                                                     |");
+        System.out.println("|                                                     |");
+        System.out.println("|      _______                          _______       |");
+        System.out.println("|_______|  0  |__________________________|  0  |______|");
+
+
+    }
+}
